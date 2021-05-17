de8446a8ead83b339ffc5c7b76897011c80f0c90
VioletViola <vivian.v18723@gmail.com>
2021-05-17 20:48:56 +0100

    Remove internal js from html file.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    
+
     // dividers
     let firstNumber = document.getElementById("firstNumber").textContent;
     let secondNumber = document.getElementById("secondNumber").textContent;
@@ -8,16 +8,36 @@ document.addEventListener("DOMContentLoaded", function () {
     // input answer field
     let response = document.getElementById("response");
     response.value = answer;
-    // submit button
-    let btn = document.getElementsByTagName('button')[0];
+
    
-    btn.addEventListener("click", function() {
-        console.log("this worked yay!");
+    let $form = $('form');
+    $form.submit(function(){
+       $.post($(this).attr('action'), $(this).serialize(), function(){
+            // submit button
+            let btn = document.getElementsByTagName('button')[0];   
+            btn.addEventListener("click", function () {
+                document.getElementsByClassName("challenge-search")[0].submit();
+            });
+       },'json');
+       return false;
     });
-    
-    console.log("firstNumber: " + firstNumber);
-    console.log("secondNumber: " + secondNumber);
-    console.log("answer: " + answer);
-    console.log("response.value: " + response.value);
+     
+
+    // // dividers
+    // let firstNumber = document.getElementById("firstNumber").textContent;
+    // let secondNumber = document.getElementById("secondNumber").textContent;
+    // // answer
+    // let answer = firstNumber / secondNumber;
+    // // input answer field
+    // let response = document.getElementById("response");
+    // response.value = answer;
+    // // submit button
+    // let btn = document.getElementsByTagName('button')[0];
+
+    // btn.addEventListener("click", function () {
+    //     console.log("this worked yay!");
+    //     document.getElementsByClassName("challenge-search")[0].submit();
+    // });
+
 
 });
