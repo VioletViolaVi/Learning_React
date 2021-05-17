7e3d31cd5867e1aeb5f97b1fb7b0a0ca6e53aeeb
VioletViola <vivian.v18723@gmail.com>
2021-05-17 21:48:07 +0100

    Edit js file.

--- scripts.js
+++ scripts.js
@@ -8,36 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // input answer field
     let response = document.getElementById("response");
     response.value = answer;
-
-   
-    let $form = $('form');
-    $form.submit(function(){
-       $.post($(this).attr('action'), $(this).serialize(), function(){
-            // submit button
-            let btn = document.getElementsByTagName('button')[0];   
-            btn.addEventListener("click", function () {
-                document.getElementsByClassName("challenge-search")[0].submit();
-            });
-       },'json');
-       return false;
-    });
-     
-
-    // // dividers
-    // let firstNumber = document.getElementById("firstNumber").textContent;
-    // let secondNumber = document.getElementById("secondNumber").textContent;
-    // // answer
-    // let answer = firstNumber / secondNumber;
-    // // input answer field
-    // let response = document.getElementById("response");
-    // response.value = answer;
-    // // submit button
-    // let btn = document.getElementsByTagName('button')[0];
-
-    // btn.addEventListener("click", function () {
-    //     console.log("this worked yay!");
-    //     document.getElementsByClassName("challenge-search")[0].submit();
-    // });
-
+    // submit button
+    let btn = document.getElementsByTagName('button')[0];
 
 });
