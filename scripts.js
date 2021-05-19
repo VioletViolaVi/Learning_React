21b42164880be613164bfe3c8f3baf9a9c32407f
VioletViola <vivian.v18723@gmail.com>
2021-05-19 12:25:42 +0100

    Create a submission function.

--- scripts.js
+++ scripts.js
@@ -1,16 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
+    let firstNumber = document.getElementById("firstNumber").textContent;
+    let secondNumber = document.getElementById("secondNumber").textContent;
+    let answer = firstNumber / secondNumber;
 
-
-    function division() {
-        // dividers
-        let firstNumber = document.getElementById("firstNumber").textContent;
-        let secondNumber = document.getElementById("secondNumber").textContent;
-        // answer
-        let answer = firstNumber / secondNumber;
-        // input answer field
+    function inputAnswer() {
         let response = document.getElementById("response");
         response.value = answer;
     }
-    division();
+    inputAnswer();
+
+    function submitAnswer() {
+        if (answer !== 1.6434977578475336) {
+            let challengeError = document.getElementsByClassName("challenge-error")[0];
+            challengeError.style.display = "block";
+        }
+    }
+    submitAnswer();
 
 });
