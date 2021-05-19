a3115a801ab83fb7141b5a587053cbc02b67b48a
VioletViola <vivian.v18723@gmail.com>
2021-05-19 11:52:06 +0100

    Refactor code.

--- scripts.js
+++ scripts.js
@@ -1,22 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    // dividers
-    let firstNumber = document.getElementById("firstNumber").textContent;
-    let secondNumber = document.getElementById("secondNumber").textContent;
-    // answer
-    let answer = firstNumber / secondNumber;
-    // input answer field
-    let response = document.getElementById("response");
-    response.value = answer;
-    // error message
-    let errorMessage = document.getElementsByClassName('challenge-error')[0];
-    // submit btn
-    let btn = document.getElementsByTagName("button")[0];
-    
-    btn.addEventListener("click", function () {
-        if (response.value === answer) {
-            errorMessage.style.display = "block";
-        }
-    })
+
+    function division() {
+        // dividers
+        let firstNumber = document.getElementById("firstNumber").textContent;
+        let secondNumber = document.getElementById("secondNumber").textContent;
+        // answer
+        let answer = firstNumber / secondNumber;
+        // input answer field
+        let response = document.getElementById("response");
+        response.value = answer;
+    }
+    division();
 
 });
