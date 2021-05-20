6863a90dec64764167719937681c3492e69d631b
VioletViola <vivian.v18723@gmail.com>
2021-05-20 22:17:57 +0100

    Add additional function and style.

--- learning_react/src/index.js
+++ learning_react/src/index.js
@@ -1,13 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function Intro() {
+  return (
+    <div>
+      <h1>This is a heading.</h1>
+      <p>This is a paragraph.</p>
+    </div>
+  )
+}
+
+function Colours() {
+  return (
+    <ul>
+      <li>red</li>
+      <li>yellow</li>
+      <li>blue</li>
+    </ul>
+  )
+}
+
+function Numbers() {
+  return (
+    <ol>
+      <li>one</li>
+      <li>two</li>
+      <li>three</li>
+    </ol>
+  )
+}
+
+function MyInfo() {
+  return (
+    <div>
+      <h1>My name is Vivian</h1>
+      <p>I'm 27 and I'm learning how to code using React.</p>
+      <p>My top 3 vacation spots are the following:</p>
+      <ul>
+        <li style={{ color: 'orange' }}>Singapore</li>
+        <li style={{ color: 'orange' }}>Mexico</li>
+        <li style={{ color: 'orange' }}>Taiwan</li>
+      </ul>
+    </div>
+  )
+}
+
 ReactDOM.render(
+  /*
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  */
+  <div>
+    <Intro />
+    <Colours />
+    <Numbers />
+    <MyInfo />
+  </div>,
+
   document.getElementById('root')
 );
 
