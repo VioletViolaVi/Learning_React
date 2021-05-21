1fe76e25754a3d5e841882c3bc002a124dc38ad6
VioletViola <vivian.v18723@gmail.com>
2021-05-21 15:55:16 +0100

    Style mobile version of nav bar.

--- learning_react/src/myComponents/HeaderContents/Header.js
+++ learning_react/src/myComponents/HeaderContents/Header.js
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 
 function Header() {
     return (
-        <header>
+        <header className="mobile_header">
             <Navbar />
         </header>
 
--- learning_react/src/myComponents/HeaderContents/Navbar.js
+++ learning_react/src/myComponents/HeaderContents/Navbar.js
@@ -4,8 +4,8 @@ function Navbar() {
     return (
         <nav>
             <ul>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
+                <li><a href="#home">Home</a> | </li>
+                <li><a href="#about">About</a> | </li>
                 <li><a href="#contactUs">Contact Us</a></li>
             </ul>
         </nav>
