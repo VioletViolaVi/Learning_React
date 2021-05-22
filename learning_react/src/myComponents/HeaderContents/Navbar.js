d9f00f007907ff296ce1d7a76295d7038d6946be
VioletViola <vivian.v18723@gmail.com>
2021-05-22 13:44:52 +0100

    Add styles using js variables in jsx.

--- learning_react/src/myComponents/HeaderContents/Navbar.js
+++ learning_react/src/myComponents/HeaderContents/Navbar.js
@@ -1,14 +1,27 @@
 import React from "react";
 
 const divider = " | ";
+const stylings = {
+    color: "pink",
+    backgroundColor: "lightGreen",
+    fontWeight: "700"
+};
+const aboutStyling = {
+    color: "yellow",
+    backgroundColor: "black"
+}
+let about_link = document.getElementsByClassName("about_link")[0].innerHTML;
+if (about_link === "About") {
+    aboutStyling.fontWeight = "900"    
+}
 
 function Navbar() {
     return (
         <nav>
             <ul>
-                <li><a href="#home">Home</a>{divider}</li>
-                <li><a href="#about">About</a>{divider}</li>
-                <li><a href="#contactUs">Contact Us</a></li>
+                <li><a style={{ color: "orange", backgroundColor: "yellow" }} href="#home">Home</a>{divider}</li>
+                <li><a style={aboutStyling} className="about_link" href="#about">About</a>{divider}</li>
+                <li><a style={stylings} href="#contactUs">Contact Us</a></li>
             </ul>
         </nav>
     )
